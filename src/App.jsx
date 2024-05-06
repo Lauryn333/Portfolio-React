f313f3793959423d0dc7be639db5d3e2e3daf4e1
Lauryn MARTIN <lauryn.martin333@gmail.com>
2024-05-06 12:46:15 +0200

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <NavBar />
       <Outlet />
+      <Footer />
     </>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,17 @@
+import logoGithub from "../assets/logoGitHub.png";
+import "../scss/footer.scss";
+
 function Footer() {
-  return <h1>Footer</h1>;
+  return (
+    <>
+      <footer>
+        <a target="blank" href="https://github.com/Lauryn333/Portfolio-React">
+          <img src={logoGithub} alt="logo github" />
+        </a>
+        <p>© Copyright - Lauryn MARTIN - 2024</p>
+      </footer>
+    </>
+  );
 }
 
 export default Footer;
