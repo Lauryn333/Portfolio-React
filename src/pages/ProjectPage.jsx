8e34363df3a3ae9e17f7eb0da94403d71aab97e3
Lauryn MARTIN <lauryn.martin333@gmail.com>
2024-05-07 11:14:43 +0200

    change some things in ProjectPage

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,12 +1,12 @@
 import { useParams } from "react-router-dom";
-import projets from "../js/projets";
+import projects from "../js/projets";
 
 function ProjectPage() {
   const { id } = useParams();
-  const ChosenProject = projets.find((project) => project.id === parseInt(id));
+  const ChosenProject = projects.find((project) => project.id === parseInt(id));
 
   return (
-    <section className="ProjectPageComponent">
+    <section className="project-pagecomponent">
       <h1>ProjectPage {id} </h1>
 
       <p>Description: {ChosenProject.description}</p>
