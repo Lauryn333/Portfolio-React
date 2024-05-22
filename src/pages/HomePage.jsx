9f7ab7ee3c905714873b45c77bf72fc2e4b649ea
Lauryn MARTIN <lauryn.martin333@gmail.com>
2024-05-22 23:12:21 +0200

    Ajout des technologies utilisées

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,16 +9,75 @@ function HomePage() {
       <div className="first-div-home">
         <img className="photo-Lauryn" src={Lauryn} alt="photo-Lauryn" />
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. In veritatis
-          ipsa dolor libero id consequatur explicabo at ipsam similique et
-          blanditiis, delectus a ea sed ullam vero, molestiae incidunt
-          perferendis!
+          Après 1 an en tant que Technicienne de laboratoire dans le milieu
+          médical, je souhaite me reconvertir dans le domaine du développement
+          web. Actuellement en formation à la Wild Code School, je recherche une
+          alternance de 15 mois à partir de septembre 2024.
         </p>
       </div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam ipsam
-        excepturi vitae itaque fuga id quos minus quam impedit deleniti!
-      </p>
+      <h2>Technologies utilisées </h2>
+      <div className="techno">
+        <h3>Front-End : </h3>
+        <div className="sous-unite">
+          <img
+            src="https://www.wildcodeschool.com/hubfs/markentive/favicons/skills/HTML5.svg"
+            alt="logo html5"
+          />
+          <p>HTML5</p>
+        </div>
+
+        <div className="sous-unite">
+          <img
+            src="https://www.wildcodeschool.com/hubfs/markentive/favicons/skills/CSS3.svg"
+            alt="logo CSS 3 "
+          />
+          <p>CSS 3</p>
+        </div>
+
+        <div className="sous-unite">
+          <img
+            src="https://www.wildcodeschool.com/hubfs/markentive/favicons/skills/Javascript.svg"
+            alt="logo JavaScript "
+          />
+          <p>JavaScript</p>
+        </div>
+
+        <div className="sous-unite">
+          <img
+            src="https://www.wildcodeschool.com/hubfs/markentive/favicons/skills/React.svg"
+            alt="logo React"
+          />
+          <p>React</p>
+        </div>
+      </div>
+
+      <div className="techno">
+        <h3>Back-End :</h3>
+        <div className="sous-unite">
+          <img
+            src="https://www.wildcodeschool.com/hubfs/markentive/favicons/skills/SQL.svg"
+            alt="logo SQL"
+          />
+          <p>SQL</p>
+        </div>
+
+        <div className="sous-unite">
+          <img
+            src="https://www.wildcodeschool.com/hubfs/markentive/favicons/skills/NodeJS.svg"
+            alt="logo Node.js"
+          />
+          <p>Node.js</p>
+        </div>
+
+        <div className="sous-unite">
+          <img
+            src="https://www.wildcodeschool.com/hs-fs/hubfs/234b110fb32958f68f318e13c7a0610e.png?width=28&height=28&name=234b110fb32958f68f318e13c7a0610e.png"
+            alt="logo Express"
+          />
+          <p>Express</p>
+        </div>
+      </div>
+
       <Link className="liste-navBar" to="/contact">
         <button className="button-home">Me contacter</button>
       </Link>
